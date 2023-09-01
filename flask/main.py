7a52ab88f3daca9d773554d56f2aaa4a39c8c523
Agustín Da Silva <agustindasilvar.dev@gmail.com>
2023-09-01 19:51:01 +0200

    sql port changed

--- flask/main.py
+++ flask/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abc123.'
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3386/ornatum'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/ornatum'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # jwt = JWTManager(app)
 
